8aa01c92bc766d63ec71d71c09fb9870d01525a7
eecheverry <eecheverry@nubark.com>
2018-12-06 15:48:50 -0500

    Add next and previous attributes to tasks

--- estimark/application/models/task.py
+++ estimark/application/models/task.py
@@ -2,3 +2,5 @@ class Task:
     def __init__(self, **attributes):
         self.id = attributes['id']
         self.name = attributes['name']
+        self.previous = attributes.get('previous')
+        self.next = attributes.get('next')
--- tests/models/test_task.py
+++ tests/models/test_task.py
@@ -2,7 +2,10 @@ from estimark.application.models import Task
 
 
 def test_task_instantiation():
-    task = Task(id='T001', name='Deploy Application Server')
+    task = Task(id='T001', name='Deploy Application Server',
+                next='T002')
 
     assert task.id == 'T001'
     assert task.name == 'Deploy Application Server'
+    assert task.previous is None
+    assert task.next == 'T002'
