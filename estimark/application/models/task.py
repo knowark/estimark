837f1d22d8d0ab9d785918b6c08fa5d3274ef66c
eecheverry <eecheverry@nubark.com>
2019-02-14 15:32:34 -0500

    Remove previous and next information from tasks as these data is handled now by links

--- estimark/application/models/task.py
+++ estimark/application/models/task.py
@@ -2,6 +2,4 @@ class Task:
     def __init__(self, **attributes):
         self.id = attributes['id']
         self.name = attributes.get('name', '')
-        self.previous = attributes.get('previous')
-        self.next = attributes.get('next')
         self.role_id = attributes.get('role_id')
--- tests/application/models/test_task.py
+++ tests/application/models/test_task.py
@@ -3,10 +3,8 @@ from estimark.application.models import Task
 
 def test_task_instantiation():
     task = Task(id='T001', name='Deploy Application Server',
-                next='T002', role_id='R1')
+                role_id='R1')
 
     assert task.id == 'T001'
     assert task.name == 'Deploy Application Server'
-    assert task.previous is None
-    assert task.next == 'T002'
     assert task.role_id == 'R1'
