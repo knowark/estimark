d186153f69a9ad1300cba893dbc2e800f3d9cb8d
Esteban Echeverry <eecheverry@nubark.com>
2019-06-24 08:46:55 -0500

    Plot title with date

--- estimark/infrastructure/plot/altair_plot_service.py
+++ estimark/infrastructure/plot/altair_plot_service.py
@@ -1,5 +1,7 @@
+
 import altair as alt
 import pandas as pd
+from datetime import date
 from pathlib import Path
 from ...application.models import Schedule
 from ...application.repositories import SlotRepository
@@ -17,13 +19,15 @@ class AltairPlotService(PlotService):
         slots = self.slot_repository.search(
             [('schedule_id', '=', schedule.id)])
         slot_dict_list = [vars(slot) for slot in slots]
+        title = f"{schedule.name} - {date.today()}"
 
         source = pd.DataFrame(slot_dict_list)
         chart = alt.Chart(source).mark_bar().encode(
             x='start',
             x2='end',
             y='name'
-        )
+        ).properties(
+            title=title)
 
         output_file = str(Path(self.plot_dir).joinpath('chart.html'))
         chart.save(output_file)
