aedb4965de8f5be45063a83269b487b286a1ec19
Esteban Echeverry <eecheverry@nubark.com>
2020-03-27 09:16:36 -0500

    Test rst_loader custom id

--- tests/infrastructure/data/test_rst_loader.py
+++ tests/infrastructure/data/test_rst_loader.py
@@ -14,3 +14,13 @@ def test_rst_loader_load(rst_loader):
 def test_rst_loader_load_if_not_initialized(rst_loader):
     assert len(rst_loader.nodes) > 0
     assert isinstance(rst_loader.nodes, list)
+
+
+def test_rst_loader_load_custom_id(rst_loader):
+    print(rst_loader.nodes)
+    for node in rst_loader.nodes:
+        if node.get('name') == '4.1 Close Project':
+            assert node['id'] == 'custom_4.1'
+            break
+    else:
+        assert False
