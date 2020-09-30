2b4dfaac87b3e03d40dedc10103e4b35f42cbf65
eecheverry <eecheverry@knowark.com>
2020-09-29 19:11:38 -0500

    Change to absolute importS

--- estimark/__main__.py
+++ estimark/__main__.py
@@ -1,5 +1,5 @@
 """
-Authark entrypoint
+Estimark entrypoint
 """
 
 import os
@@ -9,16 +9,16 @@ from json import loads
 from pathlib import Path
 from typing import List
 from injectark import Injectark
-from .core import Config, PRODUCTION_CONFIG
-from .factories import factory_builder, strategy_builder
-from .presenters.shell import Shell
+from estimark.core import Config, PRODUCTION_CONFIG
+from estimark.factories import factory_builder, strategy_builder
+from estimark.presenters.shell import Shell
 
 
 def main(args: List[str] = None):  # pragma: no cover
     args = args or sys.argv[1:]
     config_path = Path(os.environ.get('ESTIMARK_CONFIG', 'config.json'))
     config = loads(config_path.read_text()) if config_path.is_file() else {}
-    config: Config = {**PRODUCTION_CONFIG, **config}
+    config = {**PRODUCTION_CONFIG, **config}
 
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
                         format='%(message)s')
